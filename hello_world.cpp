3bc3bef048c1d440a15fd9d753ef9ec494b725ba
kishorewolfe <40271622+kishorewolfe@users.noreply.github.com>
2020-08-14 14:02:48 +0530

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+using namespace std ;
+int main()
+{
+    cout<<"HELLO WORLD "<<endl;
+
+    cout<<"Name:Kishore R"<<endl;
+    cout<<"reg:212217205023"<<endl;
+    cout<<"IT A"<<endl;
+
+
+}
